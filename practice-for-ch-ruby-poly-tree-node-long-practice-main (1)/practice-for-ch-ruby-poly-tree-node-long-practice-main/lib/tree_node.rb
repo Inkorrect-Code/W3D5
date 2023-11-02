59950cb83c27e93a6049a1de958b0f20d3ab70cf
Inkorrect-Code <ntongprofessional@gmail.com>
2023-11-02 10:29:44 -0400

    created parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main (1)/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main (1)/practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,6 +1,7 @@
 class PolyTreeNode
 
     attr_reader :value, :parent, :children
+    attr_writer :children
 
     def initialize(value)
         @value = value
@@ -8,7 +9,26 @@ class PolyTreeNode
         @children = []
     end
 
-    def parent=(value)
+    def parent=(new_parent)
+        old_parent = @parent # A's parent is currently C
+        @parent = new_parent
+
+
+        if !old_parent.nil? 
+            old_parent.children = old_parent.children.reject{ |child| self == child }
+        end
+
         
+        if !new_parent.nil? && !new_parent.children.include?(self)
+            new_parent.children << self
+        end
+        
+
+        
+
     end
+
+    # def add_child(new_parent)
+    #     new_parent.children << self
+    # end
 end
